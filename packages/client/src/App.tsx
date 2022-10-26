b04b2a8774814682dbbcd2d7401eaaea025bca4e
leaderdsn <leaderdsn.it@gmail.com>
2022-10-26 22:55:38 +0800

    moved the App to the root of src

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import { Template } from './components/template'
+
+function App() {
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchServerData()
+  }, [])
+  return <div className="app">
+    <Template />
+  </div>
+}
+
+export default App
