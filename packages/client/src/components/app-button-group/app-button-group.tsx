6ca1b4725319d7eda279544a49f9b90f7ee764fa
leaderdsn <leaderdsn.it@gmail.com>
2022-10-31 23:46:38 +0800

    layout of the app-button-group component

---
+++ packages/client/src/components/app-button-group/app-button-group.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import './app-button-group.sass'
+
+interface ButtonGroupProps {
+  buttons: string[]
+  buttonClick: (event: any) => void
+}
+
+export const AppButtonGroup:React.FC<ButtonGroupProps> = ({ buttons, buttonClick }) => {
+
+  const [clickedId, setClickedId] = useState(-1)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: React.SetStateAction<number>) => {
+    setClickedId(id);
+    buttonClick(event);
+  };
+
+  return (
+    <div className="app-button-group">
+      {buttons.map((buttonLabel: string, i) => (
+        <button 
+          key={i} 
+          name={buttonLabel} 
+          onClick={(event) => handleClick(event, i)}
+          className={i === clickedId ? "app-button-group-item active" : "app-button-group-item"}
+        >
+          {buttonLabel}
+        </button>
+      ))}
+    </div>
+  )
+}
