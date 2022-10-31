2ead4e393bfa9b34cfc60380ee51db476d7f5509
leaderdsn <leaderdsn.it@gmail.com>
2022-10-31 23:49:49 +0800

    layout of the app-profile page

---
+++ packages/client/src/pages/app-profile/app-profile.tsx
@@ -0,0 +1,72 @@
+import { AppButtonGroup } from '../../components/app-button-group';
+import { AppProfileChange } from "../../components/app-profile-change";
+import { AppProfileInfo } from "../../components/app-profile-info";
+import { useEffect, useState } from "react";
+import './app-profile.sass'
+
+export interface BaseEvent {
+  target: Target
+}
+
+interface Target {
+  name?: any; 
+  files?: FileList | null
+}
+
+export type Nullable<T> = T | null
+export type EmptyValue<T> = T | undefined
+
+export interface ProfileProps {
+  data: ProfileData,
+  formDataChange?: (formData: ProfileData) => void
+}
+
+interface ProfileData {
+  avatar: EmptyValue<string>,
+  nickName: EmptyValue<string>,
+  email: EmptyValue<string>,
+  userName: EmptyValue<string>,
+  password: EmptyValue<string>,
+  newPassword: EmptyValue<string>,
+  repeatPassword: EmptyValue<string>,
+  
+}
+
+export const AppProfile = () => {
+  const [isProfileChange, setIsProfileChange] = useState(false)
+  
+  const dataObj = {
+    avatar: '',
+    nickName: 'test',
+    email: 'test@test.test',
+    userName: 'Aleksandr',
+    password: 'qwe123',
+    newPassword: 'qwe123',
+    repeatPassword: 'qwe123',
+  }
+
+  const [data, setData] = useState<ProfileData>(dataObj)
+
+  const profileSwitching = (event: BaseEvent) => {
+    const { name } = event.target
+    
+    name === 'Профиль' ? setIsProfileChange(false) : setIsProfileChange(true)
+  };
+
+  const changeFormValue = (formData: ProfileData) => {
+    setData(formData)
+    console.log(formData)
+  };
+
+  return (
+    <div className="app-profile container">
+      <AppButtonGroup 
+        buttons={["Профиль", "Изменить профиль"]}
+        buttonClick={profileSwitching }
+      />
+      {
+        isProfileChange ? <AppProfileChange data={ data } formDataChange={changeFormValue}/> : <AppProfileInfo data={ data }/>
+      }
+    </div>
+  )
+}
