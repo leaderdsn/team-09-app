e912c52567d8f2bbe3f92bdefbd9970477ad12ab
leaderdsn <leaderdsn.it@gmail.com>
2022-10-31 23:45:52 +0800

    layout of the app-avatar component

---
+++ packages/client/src/components/app-avatar/app-avatar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import noImage from '../../assets/icons/not-allow-photo.svg';
+import './app-avatar.sass'
+
+export const AppAvatar:React.FC<{src?:string, alt?: string }> = ({ src, alt }) => {
+
+  return (
+    <div className="app-avatar">
+      <img className={src ? "app-image" : "app-no-image" }
+        src={src ? src : noImage}
+        alt={alt}
+      />
+    </div>
+  )
+}
