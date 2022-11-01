a1578f2687b4499f29ca45efe07e45e43d10fb43
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-11-01 21:37:21 +0700

    feat: Switch from sass to tailwind + daisyUI component library

--- packages/client/src/components/template/template.tsx
+++ packages/client/src/components/template/template.tsx
@@ -1,14 +1,19 @@
-import './template.sass'
-
 export const Template = () => {
   return (
-    <div className="template">
+    <div className="template ">
       <h1>H1: Этот комопнент для демонстрации, нужно удалить :)</h1>
       <h2 className="template-header">
         H2: Этот комопнент для демонстрации, нужно удалить :)
       </h2>
       <strong>STRONG: Этот комопнент для демонстрации, нужно удалить :)</strong>
       <p>DEFAULT: Этот комопнент для демонстрации, нужно удалить :)</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+      <button className="btn-primary btn">Button</button>
+      <button className="btn-secondary btn">Button</button>
+      <button className="btn-accent btn">Button</button>
+      <button className="btn-ghost btn">Button</button>
+      <button className="btn-link btn">Button</button>
     </div>
-  )
-}
+  );
+};
