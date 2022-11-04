da9bffde21650757a964ad50de3be9d693ccc1e9
leaderdsn <leaderdsn.it@gmail.com>
2022-11-05 02:49:32 +0800

    added moduleNameMapper stub to just

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -4,8 +4,12 @@ dotenv.config()
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
+  testMatch: ['**/src/**/?(*.)test.{ts,tsx}'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css)$": "identity-obj-proxy"
   },
 }
