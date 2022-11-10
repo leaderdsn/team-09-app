a026e05b86617aec687114c4877df36d6ae90751
leaderdsn <leaderdsn.it@gmail.com>
2022-11-11 00:49:18 +0800

    fix

--- packages/client/src/components/header/TopHeader.tsx
+++ packages/client/src/components/header/TopHeader.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import DarkLightMode from '@/components/UI/DarkLightMode'
-import { themeChange } from 'theme-change'
+import React, { useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { themeChange } from 'theme-change';
+import DarkLightMode from '../UI/DarkLightMode';
 
 export const TopHeader: React.FC = () => {
-  
   useEffect(() => {
     themeChange(false);
   }, []);
@@ -13,32 +12,80 @@ export const TopHeader: React.FC = () => {
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          <label tabIndex={0} className="btn-ghost btn lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </label>
-          <nav tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><NavLink to={'/'}>Главная</NavLink></li>
-            <li><NavLink to={'/profile'}>Профиль</NavLink></li>
-            <li><NavLink to={'/leaderboard'}>Лидеры</NavLink></li>
-            <li><NavLink to={'/forum'}>Форум</NavLink></li>
+          <nav
+            tabIndex={0}
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <NavLink to={'/'}>Главная</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/profile'}>Профиль</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/leaderboard'}>Лидеры</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/forum'}>Форум</NavLink>
+            </li>
           </nav>
         </div>
-        <Link to={'/'} className="btn btn-ghost normal-case text-xl">Agar.io</Link>
+        <Link to={'/'} className="btn-ghost btn text-xl normal-case">
+          Agar.io
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <nav className="menu menu-horizontal p-0">
-          <NavLink className={({ isActive }) => (isActive ? "link link-primary pr-4" : "pr-4")} to={'/'}>Главная</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "link link-primary pr-4" : "pr-4")} to={'/profile'}>Профиль</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "link link-primary pr-4" : "pr-4")} to={'/leaderboard'}>Лидеры</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? "link link-primary" : undefined)} to={'/forum'}>Форум</NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
+            to={'/'}
+          >
+            Главная
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
+            to={'/profile'}
+          >
+            Профиль
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
+            to={'/leaderboard'}
+          >
+            Лидеры
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'link-primary link' : undefined)}
+            to={'/forum'}
+          >
+            Форум
+          </NavLink>
         </nav>
       </div>
       <div className="navbar-end">
         <div className="mr-2">
           <DarkLightMode />
         </div>
-        <Link to={'/game'} className="btn">Играть</Link>
+        <Link to={'/game'} className="btn">
+          Играть
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
