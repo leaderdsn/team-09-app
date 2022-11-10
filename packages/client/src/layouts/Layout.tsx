71e238f55101fed898cb8da0e9c439887fa3d5de
leaderdsn <leaderdsn.it@gmail.com>
2022-11-11 00:45:16 +0800

    format prettier

--- packages/client/src/layouts/Layout.tsx
+++ packages/client/src/layouts/Layout.tsx
@@ -1,17 +1,16 @@
-import { TopHeader } from '@/components/Header/TopHeader'
-import { Footer } from '@/components/Footer/Footer'
+
+import { Footer } from '@/components/Footer/Footer';
+import { TopHeader } from '@/components/Header/TopHeader';
 import { LayoutInterface } from '@/layouts/types';
 
-const Layout: React.FC<LayoutInterface> = ({children}) => {
-  return(
+const Layout: React.FC<LayoutInterface> = ({ children }) => {
+  return (
     <div className="relative flex min-h-screen flex-col overflow-hidden sm:py-12">
       <TopHeader />
-      <div className="md:container md:mx-auto">
-        {children}
-      </div>
+      <div className="md:container md:mx-auto">{children}</div>
       <Footer />
     </div>
-  )
+  );
 };
 
 export default Layout;
