2c90e2881ade813eaa132cdd011d574ca80b1f29
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-11-16 23:46:21 +0700

    feat(t9-55): connect store

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
+// import { Layout } from './components/body/Layout';
+import { store } from '@/store/store';
+import { Provider } from "react-redux";
 import { SwitchRoutes } from '@/router/SwitchRoutes';
-import Layout from '@/layouts/Layout';
+import  Layout  from '@/layouts/Layout';
 
 function App() {
   return (
     <>
       <div className="container mx-auto">
-        <BrowserRouter>
-          <Layout>
-            <SwitchRoutes />
-          </Layout>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Layout>
+              <SwitchRoutes />
+            </Layout>
+          </BrowserRouter>
+        </Provider>
       </div>
       {/*Ниже строка, чтобы "проходили" тесты*/}
       Вот тут будет жить ваше приложение :)
