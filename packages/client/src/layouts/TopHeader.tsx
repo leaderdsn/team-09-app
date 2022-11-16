c0647c01daca40e8ddf37c2b3a5a1af6c01efbf9
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-11-16 23:45:36 +0700

    refactor(t9-55): refactor route display

--- packages/client/src/layouts/TopHeader.tsx
+++ packages/client/src/layouts/TopHeader.tsx
@@ -2,17 +2,17 @@ import React, { useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { themeChange } from 'theme-change';
 import DarkLightMode from '@/components/ui/DarkLightMode';
+import { routes } from '@/router/routes';
 
-export const TopHeader:React.FC = () => {
+export const TopHeader: React.FC = () => {
   useEffect(() => {
     themeChange(false);
   }, []);
-
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn-ghost btn lg:hidden">
+          <label tabIndex="0" className="btn-ghost btn lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -28,55 +28,39 @@ export const TopHeader:React.FC = () => {
               />
             </svg>
           </label>
-          <nav
-            tabIndex={0}
+          <ul
+            tabIndex="0"
             className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
           >
-            <li>
-              <NavLink to={'/'}>Главная</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/profile'}>Профиль</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/leaderboard'}>Лидеры</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/forum'}>Форум</NavLink>
-            </li>
-          </nav>
+            {routes.map((route, key) => (
+              <li className="flex list-none" key={key}>
+                <NavLink
+                  to={route.path}
+                  className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
+                >
+                  {route.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
         </div>
         <Link to={'/'} className="btn-ghost btn text-xl normal-case">
           Agar.io
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <nav className="menu menu-horizontal p-0">
-          <NavLink
-            className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
-            to={'/'}
-          >
-            Главная
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
-            to={'/profile'}
-          >
-            Профиль
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
-            to={'/leaderboard'}
-          >
-            Лидеры
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'link-primary link' : undefined)}
-            to={'/forum'}
-          >
-            Форум
-          </NavLink>
-        </nav>
+        <ul className="menu menu-horizontal p-0">
+          {routes.map((route, key) => (
+            <li className="flex list-none" key={key}>
+              <NavLink
+                to={route.path}
+                className={({ isActive }) => (isActive ? 'link-primary link pr-4' : 'pr-4')}
+              >
+                {route.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="navbar-end">
         <div className="mr-2">
